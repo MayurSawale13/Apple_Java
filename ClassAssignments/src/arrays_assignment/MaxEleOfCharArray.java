0fefde19e7e7e411a6e68ddc4481f46ffdd86fd0
Mayur <Mayur@Revision-PC>
2022-12-17 12:30:10 +0530

    Array Assignment

--- ClassAssignments/src/arrays_assignment/MaxEleOfCharArray.java
+++ ClassAssignments/src/arrays_assignment/MaxEleOfCharArray.java
@@ -5,23 +5,19 @@ import java.util.Arrays;
 public class MaxEleOfCharArray {
 
 	public static void main(String[] args) {
-		char ch [] ={'A','D','E','x','Z','R','p'};
-		for(int i=0;i<ch.length;i++)
-		{
-			for(int j=0;j<ch.length-i-1;j++)
-			{
-				if(ch[j]<ch[j+1])
-				{
-					char temp=ch[j];
-					ch[j]=ch[j+1];
-					ch[j+1]=temp;
+		char ch[] = { 'A', 'D', 'E', 'x', 'Z', 'R', 'p' };
+		for (int i = 0; i < ch.length; i++) {
+			for (int j = 0; j < ch.length - i - 1; j++) {
+				if (ch[j] < ch[j + 1]) {
+					char temp = ch[j];
+					ch[j] = ch[j + 1];
+					ch[j + 1] = temp;
 				}
 			}
-			
+
 		}
 		System.out.println(Arrays.toString(ch));
-		System.out.println("Maximum element of array is: "+ch[0]);
-		
+		System.out.println("Maximum element of array is: " + ch[0]);
 
 	}
 
